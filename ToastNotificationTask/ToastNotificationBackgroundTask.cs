d63184e25c918bc524a27fa684a559661aa57e51
Lucas <lucas.decarli@gmail.com>
2015-09-23 21:21:51 -0300

    Criação da Task para Background do Notification

---
+++ ToastNotificationTask/ToastNotificationBackgroundTask.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Background;
+using Windows.UI.Notifications;
+
+namespace ToastNotificationTask
+{
+    public sealed class ToastNotificationBackgroundTask : IBackgroundTask
+    {
+        public void Run(IBackgroundTaskInstance taskInstance)
+        {
+            //Inside here developer can retrieve and consume the pre-defined 
+            //arguments and user inputs;
+            var details = taskInstance.TriggerDetails as ToastNotificationActionTriggerDetail;
+            var arguments = details.Argument;
+            var input = details.UserInput["Video"];
+
+            // ...
+        }
+    }
+}
+
